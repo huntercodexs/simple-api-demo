a5fefba9636de9ae5933ff1ed8933698c3a17c80
Hunter Codexs <huntercodexs@gmail.com>
2024-10-09 13:06:24 -0300

    feat: [stabled] [work] Save progress

--- src/main/java/com/huntercodexs/simpleapidemo/SimpleApiDemoApplication.java
+++ src/main/java/com/huntercodexs/simpleapidemo/SimpleApiDemoApplication.java
@@ -1,14 +1,14 @@
 package com.huntercodexs.simpleapidemo;
 
-import com.huntercodexs.quickjson.QuickJson;
-import com.huntercodexs.quickjson.core.QuickJsonBuilder;
-import com.huntercodexs.quickjson.core.QuickJsonExtractor;
-import com.huntercodexs.simpleapidemo.dto.QuickJsonDto;
+//import com.huntercodexs.quickjson.QuickJson;
+//import com.huntercodexs.quickjson.core.QuickJsonBuilder;
+//import com.huntercodexs.quickjson.core.QuickJsonExtractor;
+//import com.huntercodexs.simpleapidemo.dto.QuickJsonDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-import java.util.HashMap;
+//import java.util.Arrays;
+//import java.util.HashMap;
 
 @SpringBootApplication
 public class SimpleApiDemoApplication {
@@ -20,8 +20,8 @@ public class SimpleApiDemoApplication {
 		//barcodeForm();
 		//slimPdf();
 		//lambdaAwsDemo();
-		quickJson1();
-		quickJson2();
+		//quickJson1();
+		//quickJson2();
 
 		SpringApplication.run(SimpleApiDemoApplication.class, args);
 	}
@@ -59,94 +59,94 @@ public class SimpleApiDemoApplication {
 
 	private static void quickJson1() {
 
-		QuickJson qj = new QuickJson();
-		QuickJsonBuilder qjBuilder = new QuickJsonBuilder();
-		QuickJsonExtractor qjExtractor = new QuickJsonExtractor();
-
-		HashMap<String, Object> map = new HashMap<>();
-		map.put("map1", "Map 1 Value Test");
-		map.put("map2", 345);
-		map.put("map3", Arrays.asList("Array 1", "Array 2", 222, "Array 3"));
-		map.put("map4", "{\"name\":\"Sarah Wiz\",\"parental\":\"friend\"}");
-
-		qj.setStrictMode(false);
-		qj.add("type", "Person");
-		qj.add("name", "John");
-		qj.add("lastname", "Smith");
-		qj.add("fullname", "John Smith Viz");
-		qj.add("age", 35);
-		qj.add("address", Arrays.asList("Street 1", "200", "New York City"));
-		qj.add("contacts", Arrays.asList("12345678", "98789789", "12424242"));
-		qj.add("numbers", Arrays.asList(1, 2, 3, 4, 5, 6));
-		qj.add("reference", "{\"name\":\"Sarah Wiz\",\"parental\":\"friend\"}");
-		qj.add("family",
-				Arrays.asList(
-						"mother", "July Smith",
-						"father", "Luis Smith",
-						Arrays.asList(
-								"sister", "Elen Smith", "age", 22
-						),
-						Arrays.asList(
-								"brother", "Igor Smith", "age", 24
-						)
-				)
-		);
-		qj.add("map", map);
-
-		String jsonResult = qj.json();
-
-		qjBuilder.setStrictMode(false);
-		QuickJsonDto build = (QuickJsonDto) qjBuilder.build(jsonResult, QuickJsonDto.class);
-
-		System.out.println("QUICK JSON 1");
-		System.out.println(build);
+//		QuickJson qj = new QuickJson();
+//		QuickJsonBuilder qjBuilder = new QuickJsonBuilder();
+//		QuickJsonExtractor qjExtractor = new QuickJsonExtractor();
+//
+//		HashMap<String, Object> map = new HashMap<>();
+//		map.put("map1", "Map 1 Value Test");
+//		map.put("map2", 345);
+//		map.put("map3", Arrays.asList("Array 1", "Array 2", 222, "Array 3"));
+//		map.put("map4", "{\"name\":\"Sarah Wiz\",\"parental\":\"friend\"}");
+//
+//		qj.setStrictMode(false);
+//		qj.add("type", "Person");
+//		qj.add("name", "John");
+//		qj.add("lastname", "Smith");
+//		qj.add("fullname", "John Smith Viz");
+//		qj.add("age", 35);
+//		qj.add("address", Arrays.asList("Street 1", "200", "New York City"));
+//		qj.add("contacts", Arrays.asList("12345678", "98789789", "12424242"));
+//		qj.add("numbers", Arrays.asList(1, 2, 3, 4, 5, 6));
+//		qj.add("reference", "{\"name\":\"Sarah Wiz\",\"parental\":\"friend\"}");
+//		qj.add("family",
+//				Arrays.asList(
+//						"mother", "July Smith",
+//						"father", "Luis Smith",
+//						Arrays.asList(
+//								"sister", "Elen Smith", "age", 22
+//						),
+//						Arrays.asList(
+//								"brother", "Igor Smith", "age", 24
+//						)
+//				)
+//		);
+//		qj.add("map", map);
+//
+//		String jsonResult = qj.json();
+//
+//		qjBuilder.setStrictMode(false);
+//		QuickJsonDto build = (QuickJsonDto) qjBuilder.build(jsonResult, QuickJsonDto.class);
+//
+//		System.out.println("QUICK JSON 1");
+//		System.out.println(build);
 	}
 
 	private static void quickJson2() {
 
-		QuickJson qj = new QuickJson();
-		QuickJsonBuilder qjBuilder = new QuickJsonBuilder();
-		QuickJsonExtractor qjExtractor = new QuickJsonExtractor();
-
-		HashMap<String, Object> map = new HashMap<>();
-		map.put("map1", "Map 1 Value Test");
-		map.put("map2", 345);
-		map.put("map3", Arrays.asList("Array 1", "Array 2", 222, "Array 3"));
-		map.put("map4", "{\"name\":\"Sarah Wiz\",\"parental\":\"friend\"}");
-
-		qj.setStrictMode(false);
-		qj.add("type", "Person");
-		qj.add("name", "John");
-		qj.add("lastname", "Smith");
-		qj.add("fullname", "John Smith Viz");
-		qj.add("age", 35);
-		qj.add("address", Arrays.asList("Street 1", "200", "New York City"));
-		qj.add("contacts", Arrays.asList("12345678", "98789789", "12424242"));
-		qj.add("numbers", Arrays.asList(1, 2, 3, 4, 5, 6));
-		qj.add("reference", "{\"name\":\"Sarah Wiz\",\"parental\":\"friend\"}");
-		qj.add("family",
-				Arrays.asList(
-						"mother", "July Smith",
-						"father", "Luis Smith",
-						Arrays.asList(
-								"sister", "Elen Smith", "age", 22
-						),
-						Arrays.asList(
-								"brother", "Igor Smith", "age", 24
-						)
-				)
-		);
-		qj.add("map", map);
-
-		String jsonResult = qj.json();
-
-		qjBuilder.setStrictMode(false);
-		QuickJsonDto quickJsonDto = (QuickJsonDto) qjBuilder.build(jsonResult, QuickJsonDto.class);
-
-		Object jsonFinal = qjBuilder.build(quickJsonDto);
-
-		System.out.println("QUICK JSON 2");
-		System.out.println(jsonFinal);
+//		QuickJson qj = new QuickJson();
+//		QuickJsonBuilder qjBuilder = new QuickJsonBuilder();
+//		QuickJsonExtractor qjExtractor = new QuickJsonExtractor();
+//
+//		HashMap<String, Object> map = new HashMap<>();
+//		map.put("map1", "Map 1 Value Test");
+//		map.put("map2", 345);
+//		map.put("map3", Arrays.asList("Array 1", "Array 2", 222, "Array 3"));
+//		map.put("map4", "{\"name\":\"Sarah Wiz\",\"parental\":\"friend\"}");
+//
+//		qj.setStrictMode(false);
+//		qj.add("type", "Person");
+//		qj.add("name", "John");
+//		qj.add("lastname", "Smith");
+//		qj.add("fullname", "John Smith Viz");
+//		qj.add("age", 35);
+//		qj.add("address", Arrays.asList("Street 1", "200", "New York City"));
+//		qj.add("contacts", Arrays.asList("12345678", "98789789", "12424242"));
+//		qj.add("numbers", Arrays.asList(1, 2, 3, 4, 5, 6));
+//		qj.add("reference", "{\"name\":\"Sarah Wiz\",\"parental\":\"friend\"}");
+//		qj.add("family",
+//				Arrays.asList(
+//						"mother", "July Smith",
+//						"father", "Luis Smith",
+//						Arrays.asList(
+//								"sister", "Elen Smith", "age", 22
+//						),
+//						Arrays.asList(
+//								"brother", "Igor Smith", "age", 24
+//						)
+//				)
+//		);
+//		qj.add("map", map);
+//
+//		String jsonResult = qj.json();
+//
+//		qjBuilder.setStrictMode(false);
+//		QuickJsonDto quickJsonDto = (QuickJsonDto) qjBuilder.build(jsonResult, QuickJsonDto.class);
+//
+//		Object jsonFinal = qjBuilder.build(quickJsonDto);
+//
+//		System.out.println("QUICK JSON 2");
+//		System.out.println(jsonFinal);
 
 	}
 
